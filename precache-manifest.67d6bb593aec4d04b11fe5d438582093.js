ebeecee5d2dc81b8cbd991aca780e68459d77088
Mearat Hou <mearatjames@gmail.com>
2018-12-13 01:26:52 -0800

    Updates

---
+++ precache-manifest.67d6bb593aec4d04b11fe5d438582093.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "858c717a2a6fbf83b231",
+    "url": "/clickygame/static/js/main.858c717a.chunk.js"
+  },
+  {
+    "revision": "2d32541665dd0028510e",
+    "url": "/clickygame/static/js/1.2d325416.chunk.js"
+  },
+  {
+    "revision": "858c717a2a6fbf83b231",
+    "url": "/clickygame/static/css/main.0e9ee6e5.chunk.css"
+  },
+  {
+    "revision": "2dfb00a46fedef30e4986314235ad26c",
+    "url": "/clickygame/index.html"
+  }
+];
