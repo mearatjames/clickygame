26debe8dc5fe9312f6bdf10c623d1f97e75118dc
Mearat Hou <mearatjames@gmail.com>
2018-12-13 01:08:41 -0800

    Completed all the required functionalities and logics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Content from "./components/content";
+import items from "./items.json";
+import Grid from '@material-ui/core/Grid';
+import "./App.css";
 
 class App extends Component {
+
+  state = {
+    items,
+    selected: [],
+    topScore: 0,
+    currentScore: 0
+  }
+
+  handleSelect = id => {
+    if (this.state.selected.indexOf(id) === -1) {
+      this.state.selected.push(id)
+      this.setState({currentScore: this.state.currentScore + 1})
+      this.state.currentScore >= this.state.topScore ? this.setState({topScore: this.state.topScore + 1}) : this.setState({topScore: this.state.topScore})
+    } else {
+      
+      this.setState({currentScore: 0, selected: []})
+    }
+    
+    const items = this.state.items.sort(() => 0.5 - Math.random())
+    this.setState({ items })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    <>
+      <h1 className="title">Clicky Game</h1>
+      <p>Current Score: {this.state.currentScore}</p>
+      <p>Top Score: {this.state.topScore}</p>
+      <Grid container >   
+      {this.state.items.map(i => 
+      <Content
+        handleSelect = {this.handleSelect}
+        key = {i.id}
+        id={i.id}
+        name={i.name}
+        image={i.image}
+      />
+        )}
+      
+      </Grid>
+    </>
+    )
   }
-}
+};
 
 export default App;
